65d996acadf94fdf03f80d4ad55145262d963b68
Andrea Boriero <andrea@hibernate.org>
2018-11-02 18:23:12 +0000

    Add maven jboss snapshot repo + renamed packages to org.hibernate.benchmarks

---
+++ orm5/src/main/java/org/hibernate/benchmarks/ORM5HQLParser.java
@@ -0,0 +1,9 @@
+package org.hibernate.benchmarks;
+
+public class ORM5HQLParser implements BenchmarkHQLParserTree {
+
+    @Override
+    public Object getHQLParser(String hqlString) {
+        return 0;
+    }
+}
--- orm5/src/main/java/org/jboss/perf/ORM5HQLParser.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.perf;
-
-public class ORM5HQLParser implements BenchmarkHQLParserTree {
-
-    @Override
-    public Object getHQLParser(String hqlString) {
-        return 0;
-    }
-}
